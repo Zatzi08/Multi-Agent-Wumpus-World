4a5cf482fd9e083ff454670a18fd1b49b3ad8d49
Luza988 <107258622+Luza988@users.noreply.github.com>
2024-12-05 12:54:20 +0100

    Map

---
+++ Project/Environment/Map.py
@@ -0,0 +1,42 @@
+from Project.Environment.env import EnvGenerator
+
+
+class Map:
+    __slots__ = ['height', 'width', 'start_pos', 'map', 'filled_map', 'agents']
+
+    def __init__(self, width, height, agents: list):
+        # extend grid to fit full tiles
+        if width % 3 != 0:
+            self.width += 3 - (self.width % 3)
+        if height % 3 != 0:
+            self.height += 3 - (self.height % 3)
+        self.start_pos = (1, 1)
+        gen = EnvGenerator(self.height, self.width)
+        gen.genByTile()
+        self.map = gen.getGrid()
+        gen.placeWorldItems()
+        self.filled_map = gen.getGrid()
+        self.agents = {}
+        for i in agents:
+            self.agents[i.name] = i
+
+    def getNeighbors(self, x, y):
+        neighbors = []
+        for xi in [-1, 0, 1]:
+            for yj in [-1, 0, 1]:
+                if abs(xi) == abs(yj):
+                    continue
+                elif 0 < x + xi < self.width and 0 < y + yj < self.height and self.map[y + yj][x + xi] is not None:
+                    neighbors.append((x + xi, y + yj))
+        return neighbors
+
+    def getEventsOnTile(self, x, y):
+        return self.filled_map()[y][x]
+
+    def getAgentInReach(self, x, y):
+        n = self.getNeighbors(x, y)
+        adjacent = []
+        for i in self.agents:
+            if i.getPos in n:
+                adjacent.append(i)
+        return adjacent
