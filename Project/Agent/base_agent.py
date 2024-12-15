9c6ceb0b1672d1ff153de79bcbc6e32d265f6c5c
Pow211 <pkrasnovska@gmail.com>
2024-12-15 19:58:29 +0100

    shoot bearbeitet und Tilecondition verwendet

--- Project/Agent/base_agent.py
+++ Project/Agent/base_agent.py
@@ -1,5 +1,5 @@
-from Simulator import Map
-from Knowledge.KnowledgeBase import KnowledgeBase
+from Project.Simulator import Map
+from Project.Knowledge.KnowledgeBase import KnowledgeBase, TileCondition
 import numpy
 
 
@@ -10,15 +10,15 @@ class base_agent():
         self.items = items
         self.goal = goal
         self.gold_capacity = 5
+        self.gold_count = 0
         self.knowledge = KnowledgeBase(name, map_width, map_height, position)
         self.visibility_range = 0
         self.gold_visibility_range = 0
         self.position = (1, 1)
 
-    def move(self,
-             direction):  #soll ich das zu einer Liste von Agenten machen, wenn ein Team irgendwo hin will? TODO: Nein
-        width = Map.shape[0]
-        height = Map.shape[1]
+    def move(self, direction):
+        height = Map.shape[0]
+        width = Map.shape[1]
         x, y = self.position
         directions = {
             "up": (x, y + 1),
@@ -28,14 +28,14 @@ class base_agent():
         }
         new_x, new_y = directions[direction]
 
-        if 0 < new_x < height and 0 < new_y < width:
-            if 2 in Map[new_y][
+        if 0 < new_x < width and 0 < new_y < height:
+            if TileCondition.WALL in Map[new_y][
                 new_x]:  #wenn keine wand (enum oder list idk) TODO: gute Frage... wahrscheinlich werden alle Infos aus der KB genommen, aber vielleicht nochmal fragen
                 # TODO: also wahrscheinlich Enum
                 print("Invalid move!")
 
             # Wumpus oderso
-            elif 4 in Map[new_y][new_x]:
+            elif TileCondition.WUMPUS in Map[new_y][new_x]:
                 if "shield" in self.items and self.health > 1:
                     self.health -= 1
                 elif "swordsman" in Map[new_y][
@@ -43,59 +43,65 @@ class base_agent():
                     pass
                 else:
                     self.agentDeath()
+
             #pit
-            elif 7 in Map[new_y][new_x]:
+            elif TileCondition.PIT in Map[new_y][new_x]:
                 self.agentDeath()
 
             #gold:wenn ein team auf gold ist, wer kriegt das gold dann? TODO: Jeder Agent für sich. Erster mit leerem Beutel oder Vote (kann diskutiert werden)
-            elif 10 in Map[new_y][new_x]:
-                if self.items.count(
-                        "gold") < self.gold_capacity:  #soll gold in items oder in eigenem counter sein? TODO: kp
-                    self.items.append("gold")
+            elif TileCondition.SHINY in Map[new_y][new_x]:
+                if self.gold_count < self.gold_capacity:
+                    self.gold_count += 1
 
             #update knowledge base? TODO: ja
 
             self.position = new_x, new_y
             return self.position
 
-    #check pit and wumpus in move: arrow or sword? -> call methods TODO: Hätte gedacht du musst nichts checken sondern nur machen
-
     def shoot(self, direction):
-        if "Arrow" in self.items and self.items[
-            1] > 0:  #kriegt jeder jetzt pfeile? range erstmal auf 3 gesetzt TODO: Range = infty = bis hit Wall oder Ding
+        if "Arrow" in self.items and self.items[1] > 0:
             # TODO: friendly fire?
             x, y = self.position
-            ranges = {
-                "up": [(x, y + 1), (x, y + 2), (x, y + 3)],
-                "down": [(x, y - 1), (x, y - 2), (x, y - 3)],
-                "right": [(x + 1, y), (x + 2, y), (x + 3, y)],
-                "left": [(x - 1, y), (x - 2, y), (x - 3, y)]
+            inAir = True
+            directions = {
+                "up": {"x": 0, "y": 1},
+                "down": {"x": 0, "y": -1},
+                "right": {"x": 1, "y": 0},
+                "left": {"x": -1, "y": 0}
             }
 
-            self.checkShoot(ranges[direction])
+            while inAir and (0 < x < Map.shape[1] and 0 < y < Map.shape[0]):
+                x += directions[direction][x]
+                y += directions[direction][y]
+                if TileCondition.SAFE not in Map[y][x]:
+                    if TileCondition.WUMPUS in Map[y][x]:
+                        #remove Wumpus from list
+                        index = Map[y][x].index(TileCondition.WUMPUS)
+                        Map[y][x].pop(index)
+                        print(self.name, "has killed a Wumpus!")
+                        inAir = False
+                        break
+
+                    elif TileCondition.WALL:
+                        inAir = False
+                        break
+
+                    #elif TileCondition.AGENT:
+                    #pass
 
         else:
             print(self.name, "has no arrows left!")
-        #if wall or wumpus then stop flying oder kann man 2 töten? TODO: 1 Pfeil = max 1 Kill
-
-    def checkShoot(self, positions):
-        for pos in positions:
-            if 0 < pos[0] < Map.shape[0] and 0 < pos[1] < Map.shape[1]:
-                if 2 in Map[pos[1]][pos[0]]:
-                    index = Map[pos[1]][pos[0]].index(2)
-                    Map[pos[1]][pos[0]].pop(index)
-                    print(self.name, "has killed a Wumpus!")
-                    #break ?
-                elif 1 in Map[pos[1]][pos[0]]:
-                    break
 
     def agentDeath(self):
+        x, y = self.position
         self.health = 0
         #lösche agent aus karte
-        index = Map[self.position[1]][self.position[0]].index(self.name)
-        Map[self.position[1]][self.position[0]].pop(index)
+        index = Map[y][x].index(self.name)  #wie wird ein Agent auf einer bestimmten Position gespeichert?
+        Map[y][x].pop(index)
         print(self.name, "has died!")
 
+#reload Method?
+#karte zeichnen Method?
 
 class hunter(base_agent):
     def __init__(self, name, health):  #Bogen als Item?
@@ -104,7 +110,7 @@ class hunter(base_agent):
 
 class cartographer(base_agent):
     def __init__(self, name, health):  #Karte als Item?
-        super().__init__(name, health, [], ["map"])
+        super().__init__(name, health, ["map", 1], ["map"])
 
     def init_knowledge(self, knowledge):  #alle haben ja theoretisch knowledge, nicht?
         self.knowledge = knowledge
