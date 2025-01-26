a81f6c14195bdc1d44cb80f29e69afaa76d99c90
Luza988 <107258622+Luza988@users.noreply.github.com>
2025-01-26 19:33:59 +0100

    print template

--- Project/visualizer.py
+++ Project/visualizer.py
@@ -1,14 +1,19 @@
 from dash import dcc, Dash, html, Input, Output, State, callback
 import dash_bootstrap_components as dbc
+
+from Project.SimulatedAgent.AgentEnums import AgentGoal
 from Project.Simulator import Simulator
 
 app = Dash()
 agent = -1
+template = '{:10s} {:15s} {:15s} {:15s}\n'
+
 simulator = Simulator(60, 60, 6, 2000)
 agents = simulator.get_agents()
 
 @callback(
     Output('graph', 'figure'),
+    Output("log", "children"),
     Input('submit-next', 'n_clicks'),
     State('agent_choice', 'value'),
     State('count', 'value'),
@@ -19,9 +24,13 @@ def update_graph(n_clicks, agent, value):
         agent = -1
     print("\nUpdate!", n_clicks, value, agent)
     plt = simulator.simulate_next_step(agent)
+    s = template.format('Name', "Gold owned", "Wumpus killed", "Map Progress")
+    print(s)
     for _ in range(value-1):
         plt = simulator.simulate_next_step(agent)
-    return plt
+    #for agentGoal in agentGoalList:
+
+    return plt, s
 
 
 def set_layout(plt):
@@ -30,8 +39,8 @@ def set_layout(plt):
             [
                 dbc.Col(html.Div(dcc.Graph(figure=plt, id="graph")), style={'width': 'auto'}),
                 dbc.Col(html.Div([
-                    html.Div(html.Label("text", id="log"),
-                             style={'width': 200, 'height': 500, 'margin-top': 150, 'overflow': 'auto'}),
+                    html.Div(dcc.Markdown(children=template.format('Name', "Gold owned", "Wumpus killed", "Map Progress"), id="log", style={'white-space':'pre'}),
+                             style={'width': 600, 'height': 500, 'margin-top': 150, 'overflow': 'auto'}),
                     html.Div(dcc.Dropdown(id='agent_choice', options=[
                         {'label': 'All', 'value': -1}] + [{'label': ag.name, 'value': ag.name} for ag in agents.values()]
                     , value=-1, style={'textAlign': 'center', 'margin-right': 15, 'width': 150})),
